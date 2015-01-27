55e3b3f2b0da3f9ed863e7ec720e28bbdfce25cc
jsihota <jsihota@gettyimages.com>
2015-01-27 12:10:02 -0800

    work on support vectors machine

--- poker/Poker.R
+++ poker/Poker.R
@@ -57,3 +57,9 @@ library(devtools)
 source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
 
 plot.nnet(fit)
+
+#support vector machine model.
+#install.packages("kernlab")
+library(kernlab)
+svm <- ksvm(hand ~ ., data = train[,-1],C = 40,cache = 40,type = "C-svc")
+svm
