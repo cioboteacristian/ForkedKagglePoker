1a765418cba0270eef80b14b43f12e9168af9283
Jagdeep Sihota <jagdeepsihota@Jagdeeps-MacBook-Pro.local>
2015-02-02 10:52:34 -0800

    ab

--- Trying.R
+++ Trying.R
@@ -1,5 +1,5 @@
-#setwd("~/git/GTMachineLearning_Assignment_1/poker")
-setwd("~/class/GTMachineLearning_Assignment_1/poker")
+setwd("~/git/GTMachineLearning_Assignment_1/poker")
+#setwd("~/class/GTMachineLearning_Assignment_1/poker")
 train <- read.csv("train.csv")
 
 head(train)
@@ -11,15 +11,15 @@ CART.fit <- rpart(hand ~., data=train,
                   control=rpart.control(minsplit=2, minbucket=1, cp=0.001))
 
 summary(CART.fit)
-plot(CART.fit)
+#plot(CART.fit)
 library(rattle)
 library(rpart.plot)
 library(RColorBrewer)
-fancyRpartPlot(CART.fit)
-plotcp(CART.fit)
+#fancyRpartPlot(CART.fit)
+#plotcp(CART.fit)
 
 ptree <- prune(CART.fit,cp=CART.fit$cptable[which.min(CART.fit$cptable[,"xerror"]),"CP"])
-plotcp(ptree)
+#plotcp(ptree)
 fancyRpartPlot(ptree)
 
 test <- read.csv("test.csv")
@@ -31,3 +31,21 @@ head(test)
 prediction <- predict(ptree, newdata=test)
 summary(prediction)
 head(prediction)
+#pre <- formatC(prediction, format="f", digits=0)
+
+#sss <- factor(pre)
+head(sss)
+head(pre)
+library(caret)
+sub <- read.csv("sampleSubmission.csv")
+sub = sub[,2:2]
+head(sub)
+#aaa <- factor(sub)
+
+
+
+
+summary(sss)
+
+confusionMatrix(sss,aaa)
+table(prediction)
