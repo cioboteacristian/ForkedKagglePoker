686c16aea0ae73b3fda06663da4b21a8f3c9bed8
jsihota <jsihota@gettyimages.com>
2015-02-10 12:04:12 -0800

    added svm best performance

--- rawData/svm.R
+++ rawData/svm.R
@@ -31,8 +31,19 @@ trainS5 = as.factor(train$S5)
 trainC5S5 = interaction(trainC5,  trainS5)
 
 trainCLASS = as.factor(train$CLASS)
+#http://en.wikibooks.org/wiki/Data_Mining_Algorithms_In_R/Classification/SVM#Case_Study
+#svm.model2 <- svm(trainCLASS ~ trainC1S1,trainC2S2,trainC3S3,trainC4S4,trainC5S5,  data =train, type="C-classification", kernel="linear", cost=1)
+#svm.pred <- predict(svm.model2, test[,-11])
+#install.packages('e1071',dependencies=TRUE)
+library(e1071)
+#tuned <- tune.svm(CLASS~., data = train, gamma = 10^(-6:-1), cost = 10^(-1:1))
 
-svm.model2 <- svm(trainCLASS ~ trainC1S1,trainC2S2,trainC3S3,trainC4S4,trainC5S5,  data =train, type="C-classification", kernel="linear", cost=1)
-
-svm.pred <- predict(svm.model2, test[,-11])
-
+# Parameter tuning of ‘svm’:
+#   
+#   - sampling method: 10-fold cross validation 
+# 
+# - best parameters:
+#   gamma cost
+# 0.1    1
+# 
+# - best performance: 0.5968057 
